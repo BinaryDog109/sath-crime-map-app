2437cebf04d14c74c13cd1f498f4004c73c3f3ca
GiraffeKOOOO <pstepien@hotmail.co.uk>
2022-05-10 22:06:57 +0100

    started work on ethnicity selection page, might have to change custom component to a form slection

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home';
+import EthnicitySelect from './pages/EthnicitySelect'
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <EthnicitySelect />
     </div>
   );
 }
