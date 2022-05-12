92e39fb67410bd9c48e9f13a5e5b47e7d5b53baa
BinaryDog109 <emperoryty@outlook.com>
2022-05-12 15:35:42 +0100

    Added stats

--- src/pages/MapPage/CrimeVictimRaceStats.js
+++ src/pages/MapPage/CrimeVictimRaceStats.js
@@ -1,5 +1,51 @@
+import { Divider, Link, List, ListItem, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import CountUp from "react-countup";
+
 export const CrimeVictimRaceStats = () => {
+  const [stats, setStats] = useState(null);
+  const url =
+    "https://open-data-cw2-api.azurewebsites.net/api/map/getGovEthnicityDistribution";
+  useEffect(() => {
+    const getRaceStats = async () => {
+      const response = await fetch(url);
+      const json = await response.json();
+      setStats(json);
+    };
+    getRaceStats();
+  }, []);
   return (
-    <div>CrimeVictimRaceStats</div>
-  )
-}
+    <div>
+      <Typography variant="caption">
+        Data from{" "}
+        <Link href="https://www.ethnicity-facts-figures.service.gov.uk/crime-justice-and-the-law/crime-and-reoffending/victims-of-crime/latest">
+          Victims of crime
+        </Link>{" "}
+      </Typography>
+      <List>
+        {stats &&
+          Object.keys(stats).map((ethnicity) => (
+            <>
+              <Divider textAlign="left">{ethnicity}</Divider>
+              <ListItem
+                sx={{
+                  justifyContent: "space-between",
+                  alignItems: "baseline",
+                }}
+              >
+                <CountUp
+                  style={{
+                    fontSize: "3rem",
+                    fontWeight: 600,
+                  }}
+                  end={stats[ethnicity] * 100}
+                />
+                %
+                <Typography ml={"auto"}>of being a victim of crimes</Typography>
+              </ListItem>
+            </>
+          ))}
+      </List>
+    </div>
+  );
+};
